0ed61cb2f6c321ee37c4bd72f01cd90494d547bf
ohu <otto_hu@sfu.ca>
2019-02-23 15:45:06 -0800

    setup selenium

--- BE/server.js
+++ BE/server.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 const httpServer = http.createServer(app);
 const path = require('path');
-
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 // utilities
 const bodyParser = require("body-parser");
@@ -17,12 +17,21 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 
-app.get("*", (req, res, next) => {
+app.get("/", (req, res, next) => {
+	(async function example() {
+		let driver = await new Builder().forBrowser('chrome').build();
+		await driver.get('http://www.google.com/');
+		await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+		await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+	})();
+	console.log(req.body);
 	res.sendFile(path.join(__dirname, '../FE/index.html'));
 });
 
 app.get("/search", (req, res, next) => {
 	//open browser 
+	console.log(req.body);
+
 	// call photo api
 	// return photo to client 
 });
