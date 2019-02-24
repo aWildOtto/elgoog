8dcc3b6e819741a4192e2942cb0838b5f3c1fcda
ohu <otto_hu@sfu.ca>
2019-02-24 12:27:19 -0800

    fix photo taking timing

--- BE/server.js
+++ BE/server.js
@@ -28,43 +28,44 @@ app.get("/", (req, res, next) => {
 app.post("/search", (req, res, next) => {
 	//open browser
 	console.log(req.body);
-	(async function searchKey() {
+	var searchPromise = (async function searchKey() {
 		let driver = await new Builder().forBrowser('chrome').build();
 		driver.manage().window().maximize();
 		await driver.get('http://www.google.com/');
 		await driver.findElement(By.name('q')).sendKeys(req.body.searchKey, Key.RETURN);
-		await driver.wait(until.titleIs('webdriver - Google Search'));
+		return driver.wait(until.titleIs(req.body.searchKey + ' - Google Search'));
 	})();
-	// call photo api
-	axios.post('http://localhost:8081/photo')
-		.then(response => {
-			// console.log(response.data);
-			var buff = new Buffer(response.data, 'base64');
-			fs.writeFileSync('public/resultPhoto.jpg', buff);
-			var resultText="sadfsdf";
-			var promise = (async function convertToText() {
-				let driver = await new Builder().forBrowser('chrome').build();
-				driver.manage().window().maximize();
-				await driver.get('https://www.onlineocr.net/');
-				var fullPath = __dirname + "/public/resultPhoto.jpg";
-				await driver.findElement(By.id('fileupload')).sendKeys(fullPath);
-				await driver.wait(until.elementIsEnabled(driver.findElement(By.css("input[type='submit']"))));
-				await driver.findElement(By.css("input[type='submit']")).click();
-				await driver.wait(until.elementLocated(By.id("MainContent_txtOCRResultText")));
-				await driver.findElement(By.id("MainContent_txtOCRResultText")).getText().then(text => {
-					console.log(text);
-					resultText = text;
-				});
-				return driver.quit();
-			})();
-			promise.then(()=>{
-				res.status(200).send(resultText);
+	searchPromise.then(() => {
+		// call photo api
+		axios.post('https://c505095d.ngrok.io/photo')
+			.then(response => {
+				// console.log(response.data);
+				var buff = new Buffer(response.data, 'base64');
+				fs.writeFileSync('public/resultPhoto.jpg', buff);
+				var resultText = "sadfsdf";
+				var promise = (async function convertToText() {
+					let driver = await new Builder().forBrowser('chrome').build();
+					driver.manage().window().maximize();
+					await driver.get('https://www.onlineocr.net/');
+					var fullPath = __dirname + "/public/resultPhoto.jpg";
+					await driver.findElement(By.id('fileupload')).sendKeys(fullPath);
+					await driver.wait(until.elementIsEnabled(driver.findElement(By.css("input[type='submit']"))));
+					await driver.findElement(By.css("input[type='submit']")).click();
+					await driver.wait(until.elementLocated(By.id("MainContent_txtOCRResultText")));
+					await driver.findElement(By.id("MainContent_txtOCRResultText")).getText().then(text => {
+						console.log(text);
+						resultText = text;
+					});
+					return driver.quit();
+				})();
+				promise.then(() => {
+					res.status(200).send(resultText);
+				})
 			})
-
-		})
-		.catch(error => {
-			console.log(error);
-		});
+			.catch(error => {
+				console.log(error);
+			});
+	});
 
 	// return photo to client
 	// console.log(req.body);
